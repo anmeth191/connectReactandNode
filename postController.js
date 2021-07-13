05efdf9860b9503ae71482ceb33eb039bd50170a
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-07-12 22:10:59 -0400

    Add files via upload
    today i have leanred how to use a middleware but sitll i can not update in real time in react when a http response is sent to the client, i tried using set time out but the problem each time it reaload in a interval of 5000 ms makes a new request to the server

--- postController.js
+++ postController.js
@@ -5,14 +5,33 @@ const UserData = require('../model/userData');
 
 module.exports = (app)=>{
 
-app.post('/' , (request , response) =>{
+app.post('/' , authUser,(request , response) =>{
     response.json({
         status:201,
         message:'Your post has been created',
         body: request.body
     })//end of the response 
 
-    UserData.userData.user.push(request.body)
 })//end of the post controller
 
 }//end of the full module controller
+
+
+function authUser(request , response , next){
+
+const emailSent  = request.body.emailUser;
+
+     const verifyEmail = UserData.userData.user.find( element =>{    
+        return element.emailUser === emailSent
+     })
+
+     if(verifyEmail === undefined){
+        UserData.userData.user.push(request.body)
+        next();
+     }else{
+         response.send('user exist already')
+     }
+
+
+}
+
