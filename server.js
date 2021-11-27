678ce61a1c5d321c6de927fec21216a0f714cb42
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-11-26 23:12:38 -0500

    Add files via upload
    trying to understand the cors , still having issues

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const cors = require('cors')
-
+const bodyParser = require('body-parser');
 
 
 
@@ -16,25 +16,55 @@ app.use(session({
 
 }))
 
-const checkCors = app.use((request , response , next) =>{
-   response.header("Access-Control-Allow-Origin" , "http://localhost:3000");
-   next();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.json());
 
+//create the access to cors from the client the origin is http://127.0.0.1:3000
+
+
+//  app.use((request, response , next)=>{
+//     response.header('Access-Control-Allow-Origin','*');
+//     next();
+// })
+// app.use(cors({
+//     origin:["http://127.0.0.1:3000"],
+//     methods:["GET","POST"],
+//     credentials:false,
+//     optionsSuccessStatus:200
+
+// }))
+app.use((request , response , next) =>{
+    response.header('Access-Control-Allow-Origin' , '*');
+    next();
 })
 
-
-
-app.get('/', (request , response ) =>{
+app.get('/',(request , response ) =>{
     response.send('Hello user')
 })
 
 
-app.get('/login' , (request , response) =>{
-    
-
+app.get('/login',(request , response) =>{
     response.json({
         message:'Hello from the login component'
     })
+})
+
+
+app.post('/login' , ( request , response ) =>{
+
+    const {name , pass } = request.body;
+
+  console.log(request)
+  if(name === "angel"){
+  response.json({
+      message:"hello"
+  })
+}else{
+      console.log("no hay nada")
+  }
+
+
 })
 
 app.listen(8000 , ()=>{
